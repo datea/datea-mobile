e7d88611d9f73bee299366588ca740fffc2b00ba
rdrg <rodrigoderteano@gmail.com>
2015-10-12 08:57:45 -0300

    cordova reqs

--- install-cordova-plugins.sh
+++ install-cordova-plugins.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+mkdir www plugins platforms
 cordova plugin add com.ionic.keyboard https://github.com/oauth-io/oauth-phonegap cordova-plugin-camera cordova-plugin-device cordova-plugin-file cordova-plugin-file-transfer cordova-plugin-geolocation cordova-plugin-inappbrowser cordova-plugin-splashscreen cordova-plugin-whitelist nl.x-services.plugins.socialsharing 
